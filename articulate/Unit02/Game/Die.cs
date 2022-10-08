ad3d16d9fbdff9f9d8f86763a8c704bd695be30e
clearanceyeap <clearanceleo@gmail.com>
2022-10-09 01:55:20 +0800

    Update Die.cs

--- articulate/Unit02/Game/Die.cs
+++ articulate/Unit02/Game/Die.cs
@@ -14,49 +14,17 @@ namespace Unit02.Game
         /// </summary> 
     public class Die
     {
-        public int _value; //represent 1-6 value of the the die
-        public int _points; // represent the value of the die
+        public int value;
 
     // 2) Create the class constructor. Use the following method comment.
 
         /// <summary>
         /// Constructs a new instance of Die.
         /// </summary>
-        public Die()
+    public void Roll()
         {
-           _value = 0;
-           _points = 0; 
+            Random random = new Random();
+            value = random.Next(1,14);
         }
-    
-    // 3) Create the Roll() method. Use the following method comment.
-        
-        /// <summary>
-        /// Generates a new random value and calculates the points for the die. Fives are 
-        /// worth 50 points, ones are worth 100 points, everything else is worth 0 points.
-    
-    /// </summary>
-        public void Roll()
-        {
-            Random randomGenerator = new Random();
-            _value = randomGenerator.Next(1,13);
-
-            if (_value ==30)
-            {
-                _points = 300;
-            }
-            else if (_value ==10)
-            {
-                _points = 100;
-            }
-            else if (_value ==7.5)
-            {
-                _points = 75;
-
-            }
-            else
-            {
-                _points =0;
-            }
-        }
-    }
-}    
+    }    
+}   
